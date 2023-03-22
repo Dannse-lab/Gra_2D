cf413465c580925a7f030a26ba051195e3e225e1
Dannse-lab <daniel.grzeskow2@gmail.com>
2023-03-22 23:53:27 +0100

    Animacja postaci
    Date: 23.03.2023
    
    #Addisions
    %Changes
    $BugFixes
    !Errors/Bugs
    
    #PlayerAnimaton
     .Idle
     .Run
     .Jump
     .Crouch
    
    !Space between tiles

--- 2D Game/Assets/PlayerMovement.cs
+++ 2D Game/Assets/PlayerMovement.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
 
     bool jump = false;
-
     bool crouch = false;
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         if(Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("IsJumping", true);
         }
         if(Input.GetButtonDown("Crouch"))
         {
@@ -29,6 +33,16 @@ public class PlayerMovement : MonoBehaviour {
         {
             crouch = false;
         }
+    }
+
+    public void OnCrouching(bool IsCrouching)
+    {
+        animator.SetBool("IsCrouching", IsCrouching);
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
     }
 
     void FixedUpdate()
