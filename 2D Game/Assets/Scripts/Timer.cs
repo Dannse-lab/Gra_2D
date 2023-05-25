69eee71b9c319b3dfe7dd8817d14d03f717bef49
Dannse-lab <daniel.grzeskow2@gmail.com>
2023-05-25 23:34:46 +0200

    Timer and little changes
    Added timer [minutes]:[seconds].[deciseconds].
    Fixed gaps between textures.
    Changed piston damage from 1 to 3.
    Changed player's immortality duration from 1 to 2 seconds.
    Added player's teleportation to the start of level after death.
    Added some minor details to the map.

---
+++ 2D Game/Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+using TMPro;
+
+
+namespace TMPro.Examples
+{
+
+    public class Timer : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI m_textMeshPro; // Text Reference
+        [HideInInspector] public float seconds = 0; // Timer seconds
+        [HideInInspector] public float minutes = 0; // Timer minutes
+        [HideInInspector] public string timer; // Time value (minutes:seconds.miliseconds)
+
+        void Update()
+        {
+            seconds += 1 * Time.deltaTime; // Adding time to seconds
+            if(seconds>=60)
+            {
+                seconds -= 60;
+                minutes++;
+            }
+            timer = minutes.ToString("0") + ":" + seconds.ToString("00.0");
+            m_textMeshPro.SetText(timer); // Setting text to time
+        }
+    }
+}
